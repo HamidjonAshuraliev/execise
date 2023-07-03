23863f16c3c9fef4905d0c0dc4264680994cfcc8
HAMIDJON <khamidjonashuraliev@gmail.com>
2023-07-04 00:42:53 +0900

    BRR: Arror function, Hoisting

---
+++ exe.js
@@ -0,0 +1,34 @@
+// arrow function
+// let test = () => {
+//     console.log('salom');
+// }
+// test()
+// T=salom
+
+// hoisting
+// console.log(test);
+//  var test = 'salom'
+// test()
+//  T= undefined chiqadi
+
+//  declarationda hoisting
+// test();
+// function test(){
+//         console.log('salom');
+// }
+
+// return
+// function test(){
+//     return 'salom'
+// }
+// console.log(test());
+
+// expressionda return
+// / bundayam return bn bir xil
+
+// const numbers = [10, 20, 30, 40, 50];
+// let newArr = [];
+// numbers.map((value) => {
+//   newArr.push(value + 1);
+//   console.log(newArr[newArr.length - 1]);
+// });
